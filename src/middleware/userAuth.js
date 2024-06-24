233660443b3f1f5664d81ec41359470dba227bc7
Dgdesign <dg2danielgeraldo@gmail.com>
2024-06-24 09:27:22 +0100

    implement JWT token generation with expiration

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-const userAuthmiddleware = (req, res, next)=>{
+const userAuthmiddleware2 = (req, res, next)=>{
     const token = req.cookies.token;
-
+    console.log(token)
     if(!token){
         return res.status(401).json({message:"No token, authorization danied"});
     }
@@ -10,12 +10,35 @@ const userAuthmiddleware = (req, res, next)=>{
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.email;
-        console.log(req.user.email)
+        console.log(decoded)
         next();
     } catch (error) {
         res.status(401).json({meassege:"Token is not valid"})
     }
 }
+
+const userAuthmiddleware = async (req, res, next)=>{
+    let token;
+
+    let authHeader = req.headers.authorization || req.headers.authorization
+
+    if(authHeader && authHeader.startsWith("Bearer")){
+        try {
+            token = authHeader.split(" ")[1];
+            const decod = jwt.verify(token, process.env.JWT_SECRET)
+            const user = decod.user;
+            console.log(user);
+            next();
+        } catch (error) {
+            res.status(401).json({message:'User is not authorized',err:error.message})
+        }
+       
+    }
+
+
+    
+}
+
 
 
 module.exports = userAuthmiddleware;
