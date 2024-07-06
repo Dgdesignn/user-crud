9984ba1f51a5441de33ac4f4d433875e823f40e1
Dgdesign <dg2danielgeraldo@gmail.com>
2024-07-06 22:37:46 +0100

    user-role-and-contact-model

---
+++ src/models/contantModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const contactSchema = new mongoose.Schema({
+
+    name:{type:String, require:true, trim:true},
+    phone:{type:String, require:true,trim:true},
+    email:{type:String,trim:true, lowercase:true, default:"Sem email"},
+    adress:{type:String,trim:true,default:'Sem Endereço'},
+    user:{type:mongoose.Schema.Types.ObjectId,require:true}
+    
+},{timestamps:true});
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -13,6 +13,11 @@ const userSchema = new mongoose.Schema({
     email:{
         type:String,
         unique:true
+    },
+    role:{
+        type:String,
+        enum:['normal','admin'],
+        default:'normal'
     }
 },{timestamps:true});
 
